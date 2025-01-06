71b2203f92c58feef76758f5b347782005d81fe3
Navn <152978422+OmerEryigit7@users.noreply.github.com>
2025-01-06 15:08:56 +0100

    Fant ut av hvordan jeg fetcher bøkene fra mysql

--- app.js
+++ app.js
@@ -2,12 +2,48 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 const port = 1783
+const mysql = require('mysql2')
 
 app.use(cors())
 
 app.get('/', (req, res) => {
   res.send('Hello World! Hei Ayaz')
 })
+
+let database = mysql.createConnection({
+  host: "localhost",
+  user: "ansatt",
+  password: "passord",
+  database: "bibliotek"
+})
+
+database.connect((err) => {
+  if(err){
+    console.log("Feil", err)
+  }
+  else{
+    console.log("Funker ok")
+    database.query('SELECT * FROM bøker', (err, results) => {
+      if(err) {
+        console.log("Feil", err)
+      }
+      else {
+        console.log(results)
+      }
+    })
+  }
+})
+
+app.get('/books', (req, res) => {
+  database.query('SELECT * FROM bøker', (err, results) => {
+    if (err) {
+      console.log("Feil ved spørring:", err);
+      res.status(500).send("Feil ved henting av bøker.");
+    } else {
+      res.json(results); // Send the results to the client as JSON
+    }
+  });
+});
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`)
